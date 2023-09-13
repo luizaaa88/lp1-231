4727b53635aa3106290cc9748d6ba03bf3a44711
Mamãe esqueci de deslogar <marcolinoluiza3@gmail.com>
2023-09-13 10:52:37 -0300

    recuperação

---
+++ src/semana04/EstruturaControle.java
@@ -0,0 +1,64 @@
+package semana04;
+
+import java.text.BreakIterator;
+
+public class EstruturaControle {
+    public static void main(String[] args) {
+         int idade = 20;        
+        if (idade >= 18) {
+            System.out.println("maior de idade");
+            
+        } // if/else
+        if (condition) {
+            boolean ligado =true
+            if (ligado ==)
+            systen.out. println ("sensor ligado")
+        } else {
+            system.out. println("sen (settings -> developer settings)sor desligado")
+            //switch
+            //char,
+            //
+            //enum
+            int x = 3
+            switch(x)){
+                 case 1;
+                system.out println ("numero 1");
+                break;
+                case 2;
+                system.out. println ("numero 2");
+                Break;
+                   case 3
+                 systen.out. println("numero 3");
+                break;
+                default;
+                System.out.println("numero não identificado");  
+                break;
+            }
+            // while, do while, for, foreach
+            int contador = 0;
+            while (contador < 3){
+                system .out println ("ligado!");
+                contador++;
+            }while (contador< 3);
+
+            for (int i = 0; i <3; i ++){
+                System.out.println("ligado fori");
+            }
+            //arrays
+            //varios elementos em uma mesma variaǘel
+            //acesso aos dados com numero inteiro
+            // a primeira posiçao começa 0
+            //notas = [_____,_____,_____]
+                        0     1      2
+                        
+                        
+            // 3 notas 
+            double n1 =10;
+            double n2 = 5,5;
+            double n3 = 7,4
+
+
+        }
+    }
+}
+,,,
